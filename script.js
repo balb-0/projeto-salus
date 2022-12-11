607f63d6617942bcc112585395780212ff1881bc
Vitor Balbo <101026113+balb-0@users.noreply.github.com>
2022-12-11 01:29:42 -0300

    att finalização

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const toggle = window.document.querySelector(".toggle")
 const home = window.document.querySelector('.home-close')
 const sobre = window.document.querySelector('.sobre-close')
 const servicos = window.document.querySelector('.servicos-close')
+const contato = window.document.querySelector('.contato-close')
 
 
 
@@ -34,6 +35,10 @@ servicos.addEventListener('click', () => {
     toggle.setAttribute('aria-expanded', false)
 })
 
+contato.addEventListener('click', () => {
+    lista.setAttribute('data-visible', false)
+    toggle.setAttribute('aria-expanded', false)
+})
 
 const menuItems = document.querySelectorAll('.navega a[href^="#"]')
 
@@ -54,12 +59,48 @@ function scroll(event) {
     });
 }
 
+const saibaMaisMidia = window.document.querySelector('.saibamais-servicos-midia')
+const saibaMaisSeo = window.document.querySelector('.saibamais-servicos-seo')
+const saibaMaisAnuncios = window.document.querySelector('.saibamais-servicos-anuncios')
+const saibaMaisSite = window.document.querySelector('.saibamais-servicos-site')
 
-const botaoEmail = document.querySelector('.button-email')
+const fecharMidia = window.document.querySelector('.fechar-midia')
+const fecharSeo = window.document.querySelector('.fechar-seo')
+const fecharAnuncios = window.document.querySelector('.fechar-trafego')
+const fecharSite = window.document.querySelector('.fechar-site')
 
-botaoEmail.addEventListener('click', () => {
-    botaoEmail.submit(function(e) {
-        e.preventDefault()
-        return false
-    })
+
+saibaMaisMidia.addEventListener('click', () => {
+    window.document.querySelector('.midias-expanded').setAttribute('style', 'display: flex')
+})
+
+fecharMidia.addEventListener('click', () => {
+    window.document.querySelector('.midias-expanded').setAttribute('style', 'display: none')
+
+})
+
+saibaMaisSeo.addEventListener('click', () => {
+    window.document.querySelector('.seo-expanded').setAttribute('style', 'display: flex')
+})
+
+fecharSeo.addEventListener('click', () => {
+    window.document.querySelector('.seo-expanded').setAttribute('style', 'display: none')
+})
+
+saibaMaisAnuncios.addEventListener('click', () => {
+    window.document.querySelector('.trafego-expanded').setAttribute('style', 'display: flex')
+})
+
+fecharAnuncios.addEventListener('click', () => {
+    window.document.querySelector('.trafego-expanded').setAttribute('style', 'display: none')
+
+})
+
+saibaMaisSite.addEventListener('click', () => {
+    window.document.querySelector('.site-expanded').setAttribute('style', 'display: flex')
+})
+
+fecharSite.addEventListener('click', () => {
+    window.document.querySelector('.site-expanded').setAttribute('style', 'display: none')
+
 })
